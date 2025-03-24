d997cb0e941fcae257005051901c9182061ee800
DYXX-X <119934714+DYXX-X@users.noreply.github.com>
2025-03-24 16:46:00 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,72 @@
-# peripheral_buff
+# STM32外部缓冲区
+STM32 HAL库 外部缓冲区，实现数据缓冲功能，可用于UART，IIC，SPI...
+
+## 实现原理：<br/>
+HAL 库 UART，SPI，IIC等外设的结构体句柄  [xx_HandleTypeDef] 几乎都有一个成员，RxXferCount 即 Rx传输计数器。此成员每接收一个字节，值就+1，Rx传输已完成回调调用之后，值就重新计数。由此值变化特性，就可以构建一个环形缓冲区！<br/>
+
+## 代码片段：<br/>
+```
+#include "peripheral_buff.h"
+
+PeripheralBufHandle PeripheralUart1Buf;
+UART_HandleTypeDef uart1_handle;
+
+char uart1_buffer[1024];
+int uart1_buffer_len;
+
+int main()
+{
+    HAL_Init();
+    SystemClock_Config(); // 此处未实现
+    USART1_Init(921600);  // 此处未实现
+
+    // 缓冲区初始化
+    PeripheralUart1Buf = PeripheralBuffer_Init((const uint16_t *)&uart1_handle.RxXferCount, 1024);
+    if (!PeripheralUart1Buf)
+    {
+        printf("peripheral_buffer 初始化失败！(calloc();返回 NULL，检查 startup_stm32fxx_hd.s Heap_Size 值)\n");
+        printf("相关网址： https://blog.csdn.net/weixin_42518229/article/details/108574311 \n");
+    }
+    // 开启中断接收
+    HAL_UART_Receive_IT(&uart1_handle, (uint8_t *)PeripheralUart1Buf->Buf, PeripheralUart1Buf->Size);
+
+    while (1)
+    {
+
+        if (PeripheralBuffer_ReadInterval(PeripheralUart1Buf)) // 判断当前传输是否已停止,非必要
+        {
+            uart1_buffer_len = PeripheralBuffer_ReadAvailable(PeripheralUart1Buf); // 读取缓冲区内内容长度
+            if (uart1_buffer_len > 0)
+            {
+                if (uart1_buffer_len > sizeof(uart1_buffer))
+                    uart1_buffer_len = sizeof(uart1_buffer);
+                PeripheralBuffer_ReadBytes(PeripheralUart1Buf, uart1_buffer, uart1_buffer_len); // 读取缓冲区内内容
+                uart1_buffer[uart1_buffer_len] = 0;
+                printf("缓冲区数据 %s\n", uart1_buffer);
+            }
+        }
+        HAL_Delay(10);
+    }
+    return 0;
+}
+```
+
+## 效果说明：<br/>
+串口监视器发啥收啥，无卡顿！<br/>
+
+## 性能测试：<br/>
+单次发送500字节，间隔20ms，运行一小时，无卡顿，无损坏，无乱序！<br/>
+
+## 性能瓶颈：<br/>
+分配合适的RAM数值。
+
+## debug：<br/>
+1.PeripheralBuffer_Init 返回 NULL：calloc生请内存失败，减小数值，或者修改 `startup_stm32fxx_hd.s` `Heap_Size` 值 [![Heap_Size]](https://blog.csdn.net/weixin_42518229/article/details/108574311)  <br/>
+2.`uart1_buffer`大小根据实际合理分配，如`PeripheralBuffer_Init`分配值大于`uart1_buffer`需检查每次读取长度！
+3.接收大文件需要更大缓冲区
+
+## 编译
+由于项目构建于GCC编译器，而stm32常用开发环境为Keil，若源码编译失败，可尝试链接lib文件夹下的静态库，有GCC与Keil的两种版本可挑选使用！
+
+## 示例
+详细示例程序，见examples文件夹
---
+++ examples/main.c
@@ -0,0 +1,146 @@
+#include "stm32f1xx_hal.h"
+#include "stm32f1xx_it.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+#include "peripheral_buff.h"
+
+#if __GNUC__
+int _write(int file, char *ptr, int len)
+{
+    int DataIdx;
+    for (DataIdx = 0; DataIdx < len; DataIdx++)
+    {
+        while ((USART1->SR & 0X40) == 0)
+            ;
+        USART1->DR = (uint8_t)*ptr++;
+    }
+    return len;
+}
+#else
+int fputc(int ch, FILE *stream)
+{
+    while ((USART1->SR & 0X40) == 0)
+        ;
+    USART1->DR = (uint8_t)ch;
+    return ch;
+}
+#endif
+void SystemClock_Config(void)
+{
+    HAL_StatusTypeDef ret = HAL_OK;
+    RCC_OscInitTypeDef RCC_OscInitStructure;
+    RCC_ClkInitTypeDef RCC_ClkInitStructure;
+    RCC_OscInitStructure.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+    RCC_OscInitStructure.HSEState = RCC_HSE_ON;
+    RCC_OscInitStructure.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+    RCC_OscInitStructure.PLL.PLLState = RCC_PLL_ON;
+    RCC_OscInitStructure.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+    RCC_OscInitStructure.PLL.PLLMUL = RCC_PLL_MUL9;
+    ret = HAL_RCC_OscConfig(&RCC_OscInitStructure);
+    if (ret != HAL_OK)
+        Error_Handler();
+    RCC_ClkInitStructure.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
+    RCC_ClkInitStructure.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStructure.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStructure.APB1CLKDivider = RCC_HCLK_DIV2;
+    RCC_ClkInitStructure.APB2CLKDivider = RCC_HCLK_DIV1;
+    ret = HAL_RCC_ClockConfig(&RCC_ClkInitStructure, FLASH_LATENCY_2);
+    if (ret != HAL_OK)
+        Error_Handler();
+}
+
+PeripheralBufHandle PeripheralUart1Buf;
+UART_HandleTypeDef uart1_handle;
+
+void USART1_Init(int Baud)
+{
+    {
+        GPIO_InitTypeDef GPIO_Initure;
+        __HAL_RCC_GPIOA_CLK_ENABLE();
+        __HAL_RCC_USART1_CLK_ENABLE();
+        GPIO_Initure.Pin = GPIO_PIN_9;
+        GPIO_Initure.Mode = GPIO_MODE_AF_PP;
+        GPIO_Initure.Pull = GPIO_PULLUP;
+        GPIO_Initure.Speed = GPIO_SPEED_FREQ_HIGH;
+        HAL_GPIO_Init(GPIOA, &GPIO_Initure);
+        GPIO_Initure.Pin = GPIO_PIN_10;
+        GPIO_Initure.Mode = GPIO_MODE_AF_INPUT;
+        HAL_GPIO_Init(GPIOA, &GPIO_Initure);
+
+        HAL_NVIC_EnableIRQ(USART1_IRQn);
+        HAL_NVIC_SetPriority(USART1_IRQn, 3, 3);
+    }
+    uart1_handle.Instance = USART1;
+    uart1_handle.Init.BaudRate = Baud;
+    uart1_handle.Init.WordLength = UART_WORDLENGTH_8B;
+    uart1_handle.Init.StopBits = UART_STOPBITS_1;
+    uart1_handle.Init.Parity = UART_PARITY_NONE;
+    uart1_handle.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+    uart1_handle.Init.Mode = UART_MODE_TX_RX;
+    if (HAL_UART_Init(&uart1_handle) != HAL_OK)
+    {
+        Error_Handler();
+    }
+}
+
+void USART1_IRQHandler()
+{
+    HAL_UART_IRQHandler(&uart1_handle);
+}
+
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+{
+    if (huart->Instance == USART1)
+    {
+        HAL_UART_Receive_IT(&uart1_handle, (uint8_t *)PeripheralUart1Buf->Buf, PeripheralUart1Buf->Size);
+    }
+}
+
+char uart1_buffer[1024];
+int uart1_buffer_len;
+
+int main()
+{
+    HAL_Init();
+    SystemClock_Config();
+
+    USART1_Init(921600);
+
+    printf("%s\n", "\n***** 程序启动 *****\n");
+    printf("SYSCLK频率（系统时钟频率）： %ld\n", HAL_RCC_GetSysClockFreq());
+    printf("HCLK频率（AHB总线时钟频率）： %ld\n", HAL_RCC_GetHCLKFreq());
+
+    PeripheralUart1Buf = PeripheralBuffer_Init((const uint16_t *)&uart1_handle.RxXferCount, 1024);
+    if (!PeripheralUart1Buf)
+    {
+        printf("peripheral_buffer 初始化失败！(calloc();返回 NULL，检查 startup_stm32fxx_hd.s Heap_Size 值)\n");
+        printf("相关网址： https://blog.csdn.net/weixin_42518229/article/details/108574311 \n");
+    }
+    HAL_UART_Receive_IT(&uart1_handle, (uint8_t *)PeripheralUart1Buf->Buf, PeripheralUart1Buf->Size);
+
+    while (1)
+    {
+        if (PeripheralBuffer_ReadInterval(PeripheralUart1Buf))
+        {
+            uart1_buffer_len = PeripheralBuffer_ReadAvailable(PeripheralUart1Buf);
+            if (uart1_buffer_len > 0)
+            {
+                if (uart1_buffer_len > sizeof(uart1_buffer))
+                    uart1_buffer_len = sizeof(uart1_buffer);
+                printf("数据来临 %d\n", uart1_buffer_len);
+                PeripheralBuffer_ReadBytes(PeripheralUart1Buf, uart1_buffer, uart1_buffer_len);
+                uart1_buffer[uart1_buffer_len] = 0;
+                printf("%s\n", uart1_buffer);
+            }
+            else
+            {
+                printf("err %d\n", uart1_buffer_len);
+            }
+        }
+        HAL_Delay(10);
+    }
+    return 0;
+}
